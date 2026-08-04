e7fc637da4c951b3a49a0bdf80e064e052cb9f2c
Abhishek Hiremath <153622347+Savyasachi-2005@users.noreply.github.com>
2026-08-04 14:31:12 +0530

    Time: 3 ms (95.49%), Space: 43.4 MB (83.76%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,21 +1,18 @@
 class Solution {
     public String getHint(String s, String g) {
         int bull=0,cow=0;
-        int[] freq=new int[10];
+        int[] sr=new int[10];
+        int[] gu=new int[10];
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==g.charAt(i))bull++;
-            else freq[g.charAt(i)-'0']++;
-        }
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=g.charAt(i)){
-                if(freq[s.charAt(i)-'0']>0){
-                    cow++;
-                    freq[s.charAt(i)-'0']--;
-                }
+            else {
+                sr[s.charAt(i)-'0']++;
+                gu[g.charAt(i)-'0']++;
             }
         }
-        StringBuilder sb=new StringBuilder();
-        sb.append(bull).append("A").append(cow).append("B");
-        return sb.toString();
+        for(int d=0;d<10;d++){
+            cow+=Math.min(sr[d],gu[d]);
+        }
+        return bull+"A"+cow+"B";
     }
 }
