1a3907e5c902cfb9dca9068ff8ee7e95e81f1925
Abhishek Hiremath <153622347+Savyasachi-2005@users.noreply.github.com>
2026-08-11 11:26:21 +0530

    Time: 29 ms (31.27%), Space: 47.4 MB (25.58%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,13 +1,38 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans=new ArrayList<>();
-        int[] pct=new int[26];
-        for(char c:p.toCharArray())pct[c-'a']++;
-        for(int i=0;i<=s.length()-p.length();i++){
-            int[] sct=new int[26];
-            for(int j=i;j<i+p.length();j++)sct[s.charAt(j)-'a']++;
-            if(Arrays.equals(sct,pct))ans.add(i);
+        Map<Character,Integer> map=new HashMap<>();
+        int left=0,ct=p.length();
+        for(char ch:p.toCharArray())map.put(ch,map.getOrDefault(ch,0)+1);
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            int val=map.getOrDefault(ch,0);
+            if(val>0)ct--;
+            map.put(ch,val-1);
+            if(right-left+1>p.length()){
+                char leftChar=s.charAt(left);
+                int leftVal=map.getOrDefault(leftChar,0);
+                if(leftVal>=0)ct++;
+                map.put(leftChar,leftVal+1);
+                left++;
+            }
+            if(ct==0)ans.add(left);
         }
         return ans;
     }
 }
+
+// Bruteforce Solution
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         List<Integer> ans=new ArrayList<>();
+//         int[] pct=new int[26];
+//         for(char c:p.toCharArray())pct[c-'a']++;
+//         for(int i=0;i<=s.length()-p.length();i++){
+//             int[] sct=new int[26];
+//             for(int j=i;j<i+p.length();j++)sct[s.charAt(j)-'a']++;
+//             if(Arrays.equals(sct,pct))ans.add(i);
+//         }
+//         return ans;
+//     }
+// }
